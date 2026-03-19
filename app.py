8744c6aa6052e60dd4ac033d642513f3dca9627a
Triyan Bhardwaj <triyan.bhardwaj@myclimate.org>
2026-03-19 16:15:17 +0100

    Add page count

--- app.py
+++ app.py
@@ -12,14 +12,18 @@ st.set_page_config(
 st.title('Qwiki Quickly')
 st.text_input("Search", help="Please enter at least 3 characters", width=300, key="query", icon=":material/search:")
 
+node_counter = st.empty()
+node_counter.info(" pages", icon="spinner")
+
 def render_tree(tree, parents=None, d=1):
     if not isinstance(tree, list):
         tree = [tree]
-    if parents is None:
+    if parents is None: # Runs once, for root node
+        if len(st.session_state.query) >= 3:
+            tree = ut.filter_tree(tree, st.session_state.query) 
         parents = []
-    if len(st.session_state.query) >= 3:
-        tree = ut.filter_tree(tree, st.session_state.query) 
-    
+        
+    node_count = 0
     for node in tree:
         title = node["title"]
         url = node["url"]
@@ -29,9 +33,12 @@ def render_tree(tree, parents=None, d=1):
         if has_children:
             with st.expander(title, expanded=node["expanded"], icon=icon, key=url):
                 st.caption(" -> ".join(f'[{n["title"]}]({n["url"]})' for n in path))
-                render_tree(node["children"], path, d+1)
+                node_count += 1 + render_tree(node["children"], path, d+1)
         else:
             st.markdown(f"{icon} [{title}]({url})")
-            
-
-render_tree(tree)
+            node_count += 1
+    return node_count
+    
+node_count = render_tree(tree)
+with node_counter:
+    st.info(f"{node_count} pages")
